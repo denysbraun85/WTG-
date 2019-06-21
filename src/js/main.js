6b7c232f9eb286cb33cccef5f581145459a1501e
Edwardkrasava <Golineyden@gmail.com>
2019-06-21 10:33:24 +0300

    object fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,41 @@
 $(document).ready(function () {
 
+    /*====================================================================
+                                 Script for change SVG color
+ =====================================================================*/
+
+
+    $(function changeSVG() {
+        $('img.svg_color').each(function () {
+            var $img = $(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('src');
+
+            $.get(imgURL, function (data) {
+                var $svg = $(data).find('svg');
+                if (typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                if (typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass + ' replaced-svg');
+                }
+                $svg = $svg.removeAttr('xmlns:a');
+                if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                    $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+                }
+                $img.replaceWith($svg);
+            }, 'xml');
+
+        });
+    });
+
+
+    /*====================================================================
+                                     End
+     =====================================================================*/
+
+
     $(function () {
         $('.arrow_down').on('click', function () {
             $('html, body').animate({
@@ -14,18 +50,18 @@ $(document).ready(function () {
     });
 
     $(".js-select2").select2({
-        closeOnSelect : false,
+        closeOnSelect: false,
         allowHtml: true,
         allowClear: true,
-        tags: true 
+        tags: true
     });
 
-    $(".js-select2").on('click', function(){
-        if($('.select2-container--open .select2-dropdown--below').show){
+    $(".js-select2").on('click', function () {
+        if ($('.select2-container--open .select2-dropdown--below').show) {
             $('.select2-container--default, .select2-selection--multiple').is(":hover").css({
-                '-webkit-box-shadow' : 'none',
-                '-moz-box-shadow' : 'none',
-                'box-shadow' : 'none'
+                '-webkit-box-shadow': 'none',
+                '-moz-box-shadow': 'none',
+                'box-shadow': 'none'
             });
         }
     })
@@ -46,8 +82,12 @@ $('.navbar-toggle').on('click', function () {
 /*====================================================================
                          Parallax functions
  =====================================================================*/
-$('.parallax-services').parallax({imageSrc: '../img/background/bg-services.jpg'});
-$('.parallax-reasons').parallax({imageSrc: '../img/background/reason-bg.jpg'});
+$('.parallax-services').parallax({
+    imageSrc: '../img/background/bg-services.jpg'
+});
+$('.parallax-reasons').parallax({
+    imageSrc: '../img/background/reason-bg.jpg'
+});
 /*====================================================================
                                  End
  =====================================================================*/
